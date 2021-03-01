1bfa7ff2aa250d8c9f4a9ab51b5920d1affe240c
o0grip0o <ossie.bodero@outlook.com>
2021-02-28 22:19:32 -0500

    First commit attempt 5

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example
+{
+  public class Echo : WebSocketBehavior
+  {
+    protected override void OnMessage (MessageEventArgs e)
+    {
+      var msg = System.Text.Encoding.UTF8.GetString(e.RawData);
+      Console.WriteLine("Got Message: " + msg);
+      Send (msg);
+    }
+  }
+
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      var wssv = new WebSocketServer (9000);
+      wssv.AddWebSocketService<Echo> ("/echo");
+      wssv.Start ();
+      Console.ReadKey (true);
+      wssv.Stop ();
+    }
+  }
+}
